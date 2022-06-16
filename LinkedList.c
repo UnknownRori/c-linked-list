de922524fb6ba76f3efffda752bd138be27b2f7f
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-06-17 06:04:31 +0700

    add new function - `appendLast` - `print` - `createList`

---
+++ LinkedList.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+
+#define true 1
+#define false 0
+
+void appendLast(ListNode *listNode, int value)
+{
+    ListNode *newNode = createList(value);
+
+    while (true)
+    {
+        if (listNode->next == NULL)
+        {
+            listNode->next = newNode;
+            break;
+        }
+
+        listNode = listNode->next;
+    }
+}
+
+void print(ListNode *listNode)
+{
+    if (listNode->next == NULL)
+        printf("%d \n", listNode->data);
+    else
+        printf("%d, ", listNode->data);
+
+    if (listNode->next != NULL)
+        listNode->print(listNode->next);
+}
+
+/**
+ * @brief Create a List object
+ *
+ * @param initialValue
+ * @return ListNode*
+ */
+ListNode *createList(int initialValue)
+{
+    ListNode *newList;
+
+    newList = malloc(sizeof(ListNode));
+    newList->data = initialValue;
+    newList->next = NULL;
+    newList->appendLast = &appendLast;
+    newList->print = &print;
+
+    return newList;
+}
